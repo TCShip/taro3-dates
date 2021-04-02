096c18ee2d65345948230abad4f0f72d61e5982f
paul <dongpoding@gmail.com>
2021-04-02 19:02:41 +0800

    bugfix

--- src/components/TaroDates/utils/isSameDay.ts
+++ src/components/TaroDates/utils/isSameDay.ts
@@ -1,7 +1,8 @@
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 
 export default function isSameDay(a, b) {
-  if (!dayjs.isDayjs(a) || !dayjs.isDayjs(b)) return false;
+  // if (!dayjs.isDayjs(a) || !dayjs.isDayjs(b)) return false;
+  if (!a || !b) return false;
   // Compare least significant, most likely to change units first
   // dayjs's isSame clones dayjs inputs and is a tad slow
   return a.date() === b.date()
